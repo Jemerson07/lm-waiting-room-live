088a9fd0f35c7f85afd5f81ca3ef214d1d33803e
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-04-20 13:50:16 -0300

    brand: add Jemerson Santos identity constants

---
+++ lib/branding.ts
@@ -0,0 +1,5 @@
+export const BRAND_NAME = "Jemerson Santos";
+export const BRAND_SUBTITLE = "Soluções Digitais";
+export const BRAND_TAGLINE = "Painel operacional em tempo real";
+export const BRAND_LOGO_BANNER = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAyAAAADfCAIAAAD2jMZ3AAAChElEQVR42u3TwQ3AIBDAsIP9d26XIEJC9gR5ZM18AID2/t4HAAAQlyABQAAkIEgAEAAJCBIABAA
+...TRUNCATED
