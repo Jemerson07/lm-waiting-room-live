b490b7317bca419d733bf5f1ba994125bcbe133a
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-04-20 14:33:46 -0300

    Add brand constants for branding information

---
+++ lib/brand.ts
@@ -0,0 +1,5 @@
+export const BRAND_NAME = "Jemerson Santos";
+export const BRAND_SUBTITLE = "Soluções Digitais";
+export const BRAND_PRODUCT_NAME = "Jemerson Santos";
+export const BRAND_SYSTEM_DESCRIPTION =
+  "Sistema operacional com painel ao vivo e gestão de atendimentos em tempo real.";
